e6cceffcd7c44878c59eb1ad60f5e1cd2d6e18b6
Philipp <kg9dh@hotmail.ch>
2012-12-31 11:26:31 +0100

    Update ERBPlugin/src/ch/kg9dh/main/RAP.java

--- ERBPlugin/src/ch/kg9dh/main/RAP.java
+++ ERBPlugin/src/ch/kg9dh/main/RAP.java
@@ -22,7 +22,15 @@ public class RAP implements CommandExecutor {
 		
 		if(cmd.getName().equalsIgnoreCase("rap")){
 			if(p.hasPermission("erb.rap")){
-					if(plugin.rap==true){
+					if((plugin.rap==true)&&(p.equals(plugin.rapper_1)||p.equals(plugin.rapper_2))){
+						if(p.equals(plugin.rapper_1)&&plugin.session_1==false){
+							p.sendMessage(ChatColor.DARK_RED+"[ERB] "+ChatColor.RED+"You can't rap at the moment!");
+							return true;
+						}else if(p.equals(plugin.rapper_2)&&plugin.session_2==false){
+							p.sendMessage(ChatColor.DARK_RED+"[ERB] "+ChatColor.RED+"You can't rap at the moment!");
+							return true;
+						}
+												
 						String message = "";
 						for(int x=0; x<args.length; x++){
 							message = message+args[x]+" ";
